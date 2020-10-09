bcc48509f5e928d24d120a56d04fd1ec29f09e99
pkhakbaz12481 <70548958+pkhakbaz12481@users.noreply.github.com>
2020-10-09 18:58:52 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
+
+
+
